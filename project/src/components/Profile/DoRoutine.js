35d1faa2482175871aa9ba3bdffb95de4b8417bc
Lubomir Stavrev <62726686+Lubomir-Stavrev@users.noreply.github.com>
2021-04-03 17:52:45 +0300

    fixing the lack of having a function to save the actual routine

--- project/src/components/Profile/DoRoutine.js
+++ project/src/components/Profile/DoRoutine.js
@@ -1,6 +1,8 @@
 import { Fragment, useState, useEffect } from 'react';
 import profileStyle from '../Profile/Profile.module.css'
 import services from '../../server/service'
+import moment from 'moment'
+import history from '../history'
 
 export default (props) => {
 
@@ -47,43 +49,89 @@ export default (props) => {
     function exercisesRows(times, exId) {
         let all = [];
         let exerSets = [];
-
-        Object.values(exercisesAndSets)
-            .forEach(el => {
-                if (el != undefined && el.exerciseSets) {
-                    if (el.id == exId) {
-                        exerSets.push(el);
+        if (exercisesAndSets) {
+            Object.values(exercisesAndSets)
+                .forEach(el => {
+                    if (el != undefined && el.exerciseSets) {
+                        if (el.id == exId) {
+                            exerSets.push(el);
+                        }
                     }
+
+                })
+            Object.values(exerSets).forEach(row => {
+
+                if (row.exerciseSets != undefined) {
+                    row.exerciseSets.forEach(el => {
+
+                        all.push(
+                            <Fragment>
+                                <tr >
+                                    <td><input type="text" placeholder={el.weight || 'weight'} name="weight" /></td>
+                                    <td><input type="text" placeholder={el.reps || 'reps'} name="reps" /></td>
+                                    <td><input type="text" placeholder={el.notes || 'notes'} name="notes" /></td>
+                                </tr>
+                            </Fragment>
+                        )
+                    })
                 }
 
             })
+        } else {
+            for (let i = 0; i < times; i++) {
+                all.push(
+                    <Fragment>
+                        <tr >
+                            <td><input type="text" placeholder={'weight'} name="weight" /></td>
+                            <td><input type="text" placeholder={'reps'} name="reps" /></td>
+                            <td><input type="text" placeholder={'notes'} name="notes" /></td>
+                        </tr>
+                    </Fragment>
+                )
 
-        Object.values(exerSets).forEach(row => {
-
-            if (row.exerciseSets != undefined) {
-                row.exerciseSets.forEach(el => {
-
-                    all.push(
-                        <Fragment>
-                            <tr >
-                                <td><input type="text" placeholder={el.weight || 'weight'} name="weight" /></td>
-                                <td><input type="text" placeholder={el.reps || 'reps'} name="reps" /></td>
-                                <td><input type="text" placeholder={el.notes || 'notes'} name="notes" /></td>
-                            </tr>
-                        </Fragment>
-                    )
-                })
             }
+        }
 
-        })
+
 
         return all;
     }
 
+    function saveWorkout(e) {
+        e.preventDefault();
+        let exercises = e.target.lastChild.children;
+        let allTables = [];
+        let exercisesData = [];
 
+        for (let i = 0; i < exercises.length; i++) {
+            allTables.push(exercises[i].children[1]);
+        }
+
+        for (let j = 0; j < allTables.length; j++) {
+
+            let exerciseSets = [];
+            for (let k = 0; k < allTables[j].children.length; k++) {
+                exerciseSets.push(
+                    {
+                        weight: allTables[j].children[k].children[0].firstChild.value,
+                        reps: allTables[j].children[k].children[1].firstChild.value,
+                        notes: allTables[j].children[k].children[2].firstChild.value,
+                    }
+                )
+            }
+            exercisesData.push({ exerciseSets, id: allTables[j].id });
+
+        }
+        let dateNow = moment().format('MMM Do');
+        exercisesData.push({ logDate: { dateNow } })
+
+        services.saveExercises(exercisesData, routineId).catch(err => { console.log(err); })
+        services.setLastExercise(exercisesData, routineId).catch(err => { console.log(err); })
+        return history.push("/userProfile/logs");
+    }
     return (
         <Fragment>
-            <form className={profileStyle.addExerciseForm} >
+            <form onSubmit={(e) => saveWorkout(e)} className={profileStyle.addExerciseForm} >
                 <input
                     type="text"
                     name="routineName"
